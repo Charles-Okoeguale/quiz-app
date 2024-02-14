76bdcdcfb36a22ab3d235bb476f833004a7181c4
Charles <osehaeguale@gmail.com>
2024-02-14 01:37:05 +0100

    fix form validation errors

---
+++ src/constant.js
@@ -0,0 +1,18 @@
+export const subjects = [
+    { value: 'Mathematics', label: 'Mathematics' },
+    { value: 'History', label: 'History' },
+    { value: 'Science', label: 'Science' },
+    { value: 'Software Engineering', label: 'Software Engineering' },
+    { value: 'General Studies', label: 'General Studies' },
+  ];
+
+export const difficultyLevels = [
+    { value: 'Easy', label: 'Easy' },
+    { value: 'Medium', label: 'Medium' },
+    { value: 'Hard', label: 'Hard' },
+  ];
+
+export const questionTypes = [
+    { value: 'True/False', label: 'True/False' },
+    { value: 'Multiple Options', label: 'Multiple Options' },
+  ];

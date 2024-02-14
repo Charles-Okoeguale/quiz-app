955c54ed4e36395673ecc5f25307e5acf4bc83e1
Charles <osehaeguale@gmail.com>
2024-02-14 01:42:29 +0100

    make quiz app responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import { createContext, useEffect, useState } from 'react';
+import Home from './pages/Home';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+export const Context = createContext();
+export const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  }
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  }
+});
+
+
 function App() {
+
+  const [mode, setMode] = useState(() => {
+    const storedMode = localStorage.getItem('mode');
+    return storedMode || 'light'; 
+  });
+
+  useEffect(() => {
+    localStorage.setItem('mode', mode);
+  }, [mode]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <ThemeProvider theme={mode === 'dark' ? darkTheme : lightTheme}>
+          <Context.Provider value={{mode, setMode }}>
+            <Home/>
+          </Context.Provider>
+      </ThemeProvider>
+    </>
   );
 }
 
